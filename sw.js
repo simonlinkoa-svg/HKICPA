8b7e5b28b12aa623d04cf2fba867b594decdba41
simonlinkoa-svg <simonlinkoa@gmail.com>
2026-09-12 14:33:09 +0800

    首页新增今日任务 checklist(可勾选/本地保存) + SW 缓存升级

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'hkicpa-portal-v1';
+const CACHE = 'hkicpa-portal-v2';
 self.addEventListener('install', (e) => { self.skipWaiting(); });
 self.addEventListener('activate', (e) => {
   e.waitUntil(
